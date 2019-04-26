e753d1e8d03324560a25c1f9d69ebf3bc765d73c
yn-nomura <y-nomura@virtualex.co.jp>
2019-04-26 15:13:21 +0900

    jscomp

--- src/main/webapp/ExpensesList.js
+++ src/main/webapp/ExpensesList.js
@@ -14,41 +14,21 @@ function executeAjax () {
 			function(json){
 				console.log('返却値',json);
 				var tableElements ='';
-				var syainBusyo='';
-				var label='<tr>'+'<td>'+'ID'+'</td>'+'<td>'+'部署名'+'</td>';
-				var sylabel='<option value='+'0'+'>'+'選択してください'+'</option>'
-				$('#js-department').append(label);
-				$('#syainBusyo').append(sylabel);
+				var preTableElements='<tr>'+'<th>'+申請ID+'</th>'+'<th>'+申請日+'</th>'+'<th>'+更新日+'</th>'
+				+'<th>'+申請者+'</th>'+'<th>'+申請項目名+'</th>'+'<th>'+金額+'</th>'+'<th>'+ステータス+'</th>'+'</tr>';
+
 				for(var i=0;i<json.length;i++){
-					var department=json[i];
+					var expensesElemens=json[i];
 
 					tableElements+='<tr>';
 					var num = department.departmentCategory;
 					var name = department.department;
 					tableElements+='<td>'+num+'</td>'+'<td>'+name+'</td>';
-
-//					tableElements+='<td>'+'<input type="button" value="編集" class = "departmentEdit" id = de'+ (i+1) +'>'
-//					+'</td>'+'<td>'+'<input type="button" value="削除" class = "departmentDelete" id = dd'+ (i+1) +' >'+'</td>';
-					tableElements+='<td>'+'<button class="departmentEdit" id=de'+num+'>'+'編集'+'</button>'
-					+'<td>'+'<button class="departmentDelete" id=de'+num+'>'+'削除'+'</button>'
-
 					tableElements+='</tr>';
 
-//					部署データを社員編集ページに送る
-					syainBusyo+='<option value='+num+'>'+name+'</option>';
 				}
-				console.log('tableElements:',tableElements);
-				$('#js-department').append(tableElements);
-
-				console.log('部署データ',syainBusyo);
-				$('#syainBusyo').append(syainBusyo);
-
-				setClickDelBtn();
-				setClickEditBtn();
-				editBtn();
-
-
-
+				var marge=preTableElements+tableElements;
+				$('#expensesList').append(marge);
 
 			},function(XMLHttpRequest,textStatus,errorThrown){
 				alert('データの通信に失敗しました')
